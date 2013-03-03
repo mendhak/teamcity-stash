a81f5470de76161dc297e1e4b99df9fef75a5ccc
Mendhak <smaltaf@msn.com>
2013-03-03 13:56:57 +0000

    Fixed headers and logger

--- server/src/test/java/tests/StashClientTest.java
+++ server/src/test/java/tests/StashClientTest.java
@@ -15,20 +15,20 @@
  */
 
 /*
-*    This file is part of TeamCity Stash Notifier.
+*    This file is part of TeamCity Stash.
 *
-*    TeamCity Stash Notifier is free software: you can redistribute it and/or modify
+*    TeamCity Stash is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
 *    the Free Software Foundation, either version 3 of the License, or
 *    (at your option) any later version.
 *
-*    TeamCity Stash Notifier is distributed in the hope that it will be useful,
+*    TeamCity Stash is distributed in the hope that it will be useful,
 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *    GNU General Public License for more details.
 *
 *    You should have received a copy of the GNU General Public License
-*    along with TeamCity Stash Notifier.  If not, see <http://www.gnu.org/licenses/>.
+*    along with TeamCity Stash.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
